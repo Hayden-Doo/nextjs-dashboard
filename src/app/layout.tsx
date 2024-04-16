a7a4ea88bbd8d8073908f82a3a692ceddbad2de1
Hayden-Doo <heton.doo@gmail.com>
2024-04-16 11:51:15 +0900

    Chapter 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
+import { inter } from './ui/fonts';
 import './ui/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
